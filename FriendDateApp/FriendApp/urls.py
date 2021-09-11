4d27508023f3f88cd72160f5e3ec1aba7863ea0f
Kacie <kaciehawkins1@gmail.com>
2021-09-11 12:40:33 -0600

    imported views from FriendApp folder and added a path function

--- FriendDateApp/FriendApp/urls.py
+++ FriendDateApp/FriendApp/urls.py
@@ -1,2 +1,9 @@
-import views
+from django.urls.resolvers import URLPattern
+from . import views
+from django.urls import path 
+
+app_name = 'FriendApp'
+urlpatterns = [
+    path('index', views.index, name='index')
+]
 
