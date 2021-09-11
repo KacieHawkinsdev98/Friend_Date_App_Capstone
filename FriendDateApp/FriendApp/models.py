d5737c4648afd1ea6cccdf88c05cb848c3276f13
Kacie <kaciehawkins1@gmail.com>
2021-09-11 12:30:36 -0600

    added Match, Pictures, Profile, Likes, Dislikes, and Location models to models.py file

--- FriendDateApp/FriendApp/models.py
+++ FriendDateApp/FriendApp/models.py
@@ -1,16 +1,78 @@
 from django.db import models
 
+
+
 # Create your models here.
 
 class User(models.Model):
-    first_name = models.Charfield(max_length=50)
-    last_name = models.Charfield(max_length=50)
-    phone_number = models.Integerfield()
-    email = models.Charfield(max_length=50)
-    username = models.Charfield(max_length=50)
-    age = models.Integerfield()
-    location = models.Locationfield()
+    user_id = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.IntegerField()
+    email = models.EmailField(max_length=50)
+    username = models.CharField(max_length=50)
+    age = models.IntegerField()
+    #location = models.PointField(0, blank=0,)
+    gender = models.CharField(max_length=50)
+    gender_preference = models.CharField(max_length=50)
+    likes = models.IntegerField()
+    match_id =models.CharField(max_length=50)
+    interests = models.CharField(max_length=50)
+    food_preferences = models.CharField(max_length=50)
+    music_preferences = models.CharField(max_length=50)
+    bio = models.CharField(max_length=50)
+
+
+    def __str__(self):
+      return self.name
+
+class Match(models.Model):
+   match_id = models.CharField(max_length=50)
+   timestamp = models.TimeField()
+   user_id = models.CharField(max_length=50)
+
+   def __str__(self):
+      return self.name
+
     
+class Pictures(models.Model):
+  picture_id = models.IntegerField()
+  user_id =  models.CharField(max_length=50)
+
+  def __str__(self):
+      return self.name
+
+class Likes(models.Model):
+  user_id = models.CharField(max_length=50)
+  liked_user = models.CharField(max_length=50)
+
+  def __str__(self):
+      return self.name
+
+class Dislikes(models.Model):
+  user_id = models.CharField(max_length=50)
+  disliked_user = models.CharField(max_length=50)
+  
+  def __str__(self):
+      return self.name
+
+class Profiles(models.Model):
+  profile_id = models.IntegerField()
+  user_id = models.CharField(max_length=50)
+
+  def __str__(self):
+      return self.name
+
+class Location(models.Model):
+  location_id = models.IntegerField()
+  latitude = models.IntegerField()
+  longitude = models.IntegerField()
+
+  def __str__(self):
+      return self.name
+
+
+
 
 
 
