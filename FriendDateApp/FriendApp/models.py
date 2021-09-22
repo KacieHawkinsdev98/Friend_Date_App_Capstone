775103746b216fe5f174595560a7541a645ed59b
Kacie <kaciehawkins1@gmail.com>
2021-09-21 20:27:16 -0600

    created FriendRequest class in Models.py

--- FriendDateApp/FriendApp/models.py
+++ FriendDateApp/FriendApp/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils import timezone
+from django.db.models.signals import post_save
+from django.conf import settings
 
 
 
@@ -13,7 +18,7 @@ class User(models.Model):
     age = models.IntegerField()
     gender = models.CharField(max_length=50)
     bio = models.CharField(max_length=50)
-    profile = models.CharField
+    profile = models
 
 
 
@@ -45,12 +50,30 @@ class Dislikes(models.Model):
 
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
-  bio = models.CharField(max_length=500)
+  bio = models.CharField(max_length=500, blank=True)
   profile_id = models.IntegerField()
   image = models.ImageField('default.jpg', upload_to='profile_pics')
-  
+  friends = models.ManyToManyField("Profile", blank=True)
+
   def __str__(self):
      return f'{self.user.username} Profile'
+
+
+def post_save_user_model_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        try:
+            Profile.objects.create(user=instance)
+        except:
+            pass
+
+post_save.connect(post_save_user_model_receiver, sender=settings.AUTH_USER_MODEL)
+
+class FriendRequest(models.Model):
+   to_user= models.ForeignKey(settings.AUTH_USER_MODEL, related_name='to_user', on_delete=models.CASCADE)
+   from_user= models.ForeignKey(settings.AUTH_USER_MODEL, related_name='from_user', on_delete=models.CASCADE)
+   timestamp= models.DateTimeField(auto_now_add=True)
+
+
 
 
 
